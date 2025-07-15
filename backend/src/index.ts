e976ce4e5f27c34f9890c0a9a38a0ae2a337f702
fjmp20 <fco_javi_mtz@outlook.com>
2025-07-15 17:55:15 -0600

    chore: configure commitlint and husky setup

---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-console
+console.log('Medical Forms Backend - TypeScript funcionando');
+
+export const config = {
+  name: 'medical-forms-backend',
+  version: '1.0.0',
+};
